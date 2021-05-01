cdf2e04bfb54537c1055a5deb78c48ea4a8f6eda
bkfwebdev <bryant.feld@gmail.com>
2021-05-01 12:37:59 -0400

    first commit

---
+++ thebasics.js
@@ -0,0 +1,45 @@
+const AppContent = {
+    html: "https://www.youtube.com/embed/UB1O30fR-EE",
+    css: "https://www.youtube.com/embed/yfoY53QXEnI",
+    javascript: "https://www.youtube.com/embed/hdI2bqOjy3c",
+    overview: "https://www.youtube.com/embed/0pThnRneDjw",
+    supplemental: {
+        html: {
+            title:"Hyper Text Markup Language",
+            w3s: "https://www.w3schools.com/html/",
+            mdn: "https://developer.mozilla.org/en-US/docs/Web/HTML",
+            fcc: "https://www.freecodecamp.org/news/the-html-handbook/",
+            descriptivetext:"HTML (HyperText Markup Language) is the most basic building block of the Web. It defines the meaning and structure of web content. Other technologies besides HTML are generally used to describe a web page's appearance/presentation (CSS) or functionality/behavior (JavaScript)."
+        },
+        css: {
+            title:"Cascading Style Sheets",
+            w3s: "https://www.w3schools.com/css/",
+            mdn: "https://developer.mozilla.org/en-US/docs/Web/CSS/Tutorials",
+            fcc: "https://www.freecodecamp.org/news/the-css-handbook-a-handy-guide-to-css-for-developers-b56695917d11/",
+            descriptivetext:"Cascading Style Sheets (CSS) is a stylesheet language used to describe the presentation of a document written in HTML or XML (including XML dialects such as SVG, MathML or XHTML). CSS describes how elements should be rendered on screen, on paper, in speech, or on other media."
+        },
+        js: {
+            title: "JavaScript",
+            w3s: "https://www.w3schools.com/js/DEFAULT.asp",
+            mdn: "https://developer.mozilla.org/en-US/docs/Learn/Getting_started_with_the_web/JavaScript_basics",
+            fcc: "https://www.freecodecamp.org/news/learn-javascript-full-course/",
+            descriptivetext:"JavaScript (JS) is a lightweight, interpreted, or just-in-time compiled programming language with first-class functions. While it is most well-known as the scripting language for Web pages, many non-browser environments also use it, such as Node.js, Apache CouchDB and Adobe Acrobat. JavaScript is a prototype-based, multi-paradigm, single-threaded, dynamic language, supporting object-oriented, imperative, and declarative (e.g. functional programming) styles."
+        },
+        onlinebootcamps: {
+            title:"Online Education",
+            codecademy: "https://www.codecademy.com/",
+            treehouse: "https://www.teamtreehouse.com",
+            springboard: "https://www.springboard.com/courses/software-engineering-career-track",
+            descriptivetext:"Web development is the building and maintenance of websites; it's the work that happens behind the scenes to make a website look great, work fast and perform well with a seamless user experience. Web developers, or 'devs', do this by using a variety of coding languages."
+
+        }
+
+    }
+
+};
+
+/* 
+On Page load App should use content from overview & online bootcamps
+When each button is clicked update page with approriate video content, text, and links
+*/
+  
